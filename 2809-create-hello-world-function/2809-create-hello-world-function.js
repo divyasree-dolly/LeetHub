06ed5e37a3355dec026bc76a86063ab6cd0d9c41
Divyasree Bingi <67378291+divyasree-dolly@users.noreply.github.com>
2025-03-12 18:47:30 -0500

    Time: 28 ms (99.34%), Space: 53.1 MB (26.84%) - LeetHub

--- 2809-create-hello-world-function/2809-create-hello-world-function.js
+++ 2809-create-hello-world-function/2809-create-hello-world-function.js
@@ -2,9 +2,8 @@
  * @return {Function}
  */
 var createHelloWorld = function() {
-    const greeting = "Hello World";
-    return function() {
-        return greeting;
+    return function(...args) {
+        return "Hello World";
         
     }
 };
